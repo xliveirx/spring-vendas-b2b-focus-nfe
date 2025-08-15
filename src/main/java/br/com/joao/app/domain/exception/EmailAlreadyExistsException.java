1dc7c0a9a1cda441dac8a9a2e9045587ddf3754e
João Oliveira <xliveirvwrld@gmail.com>
2025-08-15 16:35:56 -0300

    Revert "feat: create global exception handler and update custom exceptions to extend it"
    This reverts commit 3ab695d3347ad75eff1d028c5c3635ba2b70121e.

--- src/main/java/br/com/joao/app/domain/exception/EmailAlreadyExistsException.java
+++ src/main/java/br/com/joao/app/domain/exception/EmailAlreadyExistsException.java
@@ -1,18 +1,7 @@
 package br.com.joao.app.domain.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
-
-public class EmailAlreadyExistsException extends DomainException {
-
-    @Override
-    public ProblemDetail toProblemDetail() {
-
-        var pb = ProblemDetail.forStatus(HttpStatus.UNPROCESSABLE_ENTITY);
-
-        pb.setTitle("Email Already Exists");
-        pb.setDetail("Email already exists in the database, try another");
-
-        return pb;
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException() {
+        super();
     }
 }
