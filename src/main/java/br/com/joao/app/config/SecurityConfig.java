6987e80e4247ca5306f91c1095e1a1f318797bfd
João Oliveira <xliveirvwrld@gmail.com>
2025-08-18 15:22:09 -0300

    feat: fix errors

--- src/main/java/br/com/joao/app/config/SecurityConfig.java
+++ src/main/java/br/com/joao/app/config/SecurityConfig.java
@@ -1,12 +1,11 @@
 package br.com.joao.app.config;
 
-import br.com.joao.app.domain.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,6 +16,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final TokenAuthenticationFilter tokenAuthenticationFilter;
--- src/main/java/br/com/joao/app/domain/exception/CompanyNameAlreadyRegisteredException.java
+++ src/main/java/br/com/joao/app/domain/exception/CompanyNameAlreadyRegisteredException.java
@@ -3,14 +3,14 @@ package br.com.joao.app.domain.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 
-public class CnpjAlreadyRegistered extends DomainException {
+public class CompanyNameAlreadyRegisteredException extends DomainException {
 
     @Override
     public ProblemDetail toProblemDetail() {
         var pb = ProblemDetail.forStatus(HttpStatus.UNPROCESSABLE_ENTITY);
 
-        pb.setTitle("CNPJ already registered.");
-        pb.setDetail("CNPJ already registered, it must be unique. Try again.");
+        pb.setTitle("Company name already registered.");
+        pb.setDetail("Company name already registered, it must be unique. Try again.");
 
         return pb;
     }
